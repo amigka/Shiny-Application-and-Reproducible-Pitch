67b87576fdbf3682c5849a04746f6b623abbc1b2
amigka <109158293+amigka@users.noreply.github.com>
2023-02-11 04:19:57 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,10 @@
 library(shiny)
 shinyUI(fluidPage(
   titlePanel("Predicting the life expectancy"),
+  p("This app allows the user show how life expectancy in related to GDP per capita"),
+  h3("Directions on how to use this interface"),
+  p("1). Use the scroll bar to select the desired GDP per capita."),
+  p("3). See the predicted life exprctancy calculated by the tool."),
   sidebarLayout(
     sidebarPanel(
       sliderInput("slidergdpPercap", "What is the life expectancy?", 0, 100000, value = 1000),
